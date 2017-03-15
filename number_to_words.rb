8d4221122caef50606938839b7550680b4013d1e
Rajiv Shrivastava <rjv834@gmail.com>
2017-03-15 17:13:31 +0530

    Update number_to_words.rb

--- number_to_words.rb
+++ number_to_words.rb
@@ -1,25 +1,51 @@
+def print_to_words_3_digits(number)
 
-numbers  = {"1": "one",  "2":  "two" , "3":  "three", "20": "twenty" , "30": "thirty"}
+ones = {"1" => "one","2" =>"two" ,"3" =>"three" ,"4" =>"four","5" =>"five","6" =>
+       "six","7" =>"seven" ,"8" =>"eight" ,"9" =>"nine","10" => "ten" ,"11" => "eleven","
+       12" =>"twelve","13"=>"thirteen","14"=> "fourteen","15"=>"fifteen","16"=>"sixteen",
+       "17"=>"seventeen","18"=>"eighteen" ,"19" =>"nineteen"}
+tens = {"20" => "twenty","30" => "thirty","40" =>"fourty","50"=>
+	"fifty","60" =>"sixty","70" =>"seventy","80" => "eighty","90" =>"ninety"}
 
+leng = number.to_s.length
 
-number = gets.to_i
+            if number < 20
+        	     return ones["#{number.to_s}"]
+            elsif number > 20
+        	    num_string = []
+        	    count = 0
+	       	        while number > 0
+	                   place = number % 10
+	                        if count == 0
+	                       	    num_string[0] = ones["#{place.to_s}"]
+	                        elsif count == 1
+	                       	    place = place * 10
+	                       	    num_string[1] = tens["#{place.to_s}"]
+	                       	elsif count == 2
+	                       		yum = ones["#{place.to_s}"]
+	                       		 place = place * 10
+	                       	    num_string[2] = yum +" " + "hundred"+ " "
+	                       	end
 
-count_digits = 0
-
-while number > 0 do 
-
-      count_digits = count_digits + 1
-
-      number = number / 10
-	
+	                    number = number /10
+	                    count = count + 1
+	                end  #while ends
+		    end
+	return num_string  
 end
-      
-puts count_digits
-      case count_digits
 
-      when 1
-      	print numbers[:1]
-     
-      else
-      	print none
-      end
+	  
+  
+num = gets.chomp
+array =  print_to_words_3_digits(num.to_i)
+
+leng = array.length
+
+for i in 0..leng
+	print "#{array[leng-i]}" + " "
+	puts " " if i==leng
+end
+
+
+
+
